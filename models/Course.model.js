595e72b1077fcc786205e33246deade9c16824c2
Vishnu <svishnu987@gmail.com>
2023-03-09 11:49:12 +0100

    models done

---
+++ models/Course.model.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require("mongoose");
+
+// TODO: Please make sure you edit the User model to whatever makes sense in this case
+const courseSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "title is required"],
+    },
+    description: {
+      type: String,
+      maxLength: 500,
+    },
+    subject: {
+      type: String,
+      required: [true, "Subject is required."],
+      enum: [
+        "Maths",
+        "Physics",
+        "Computer Science",
+        "Physical Education",
+        "History",
+        "Chemistry",
+        "Biology",
+      ],
+    },
+    teacher: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    // this second object adds extra properties: `createdAt` and `updatedAt`
+    timestamps: true,
+  }
+);
+
+const Course = model("Course", courseSchema);
+
+module.exports = Course;
